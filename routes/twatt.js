5c2f9bf85f67c0e67395bf3674960ced3265fa80
Eri Irawan <email": null>
2017-02-28 12:02:25 +0700

    add params for search and use key words

--- routes/twatt.js
+++ routes/twatt.js
@@ -13,9 +13,9 @@ let my_oAuth = new oAuth.OAuth(
   'HMAC-SHA1'
 )
 
-router.get('/search', function (req, res, next) {
+router.post('/search', function (req, res, next) {
   my_oAuth.get(
-    'https://api.twitter.com/1.1/search/tweets.json?q=liverpool&src=typd',
+    `https://api.twitter.com/1.1/search/tweets.json?q=${req.body.keyWord}&src=typd`,
     '274991751-Gn09k3MXKekv4rsNYCYUzCKgjWcUjQdoXIY7JvJo', // test user token
     'Lqnyus1kHeVMsJ89TWk0v3w08VB2cblIO4TF7tnoKos9V', // test user secret
     function (e, data, respond) {
@@ -25,7 +25,5 @@ router.get('/search', function (req, res, next) {
     }
   )
 })
-
-// https://api.twitter.com/1.1/search/tweets.json?q=hacktiv8&src=typd
 
 module.exports = router
